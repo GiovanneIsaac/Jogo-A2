bc88b83aa36cc4bc41535f2bf19dc0baa63413ed
Giovanne Isaac <126466911+GiovanneIsaac@users.noreply.github.com>
2023-12-09 19:37:39 +0000

    classe_fundo_criada

--- classes.py
+++ classes.py
@@ -1,1 +1,18 @@
 # ARQUIVO PARA CONSTRUÇÃO DAS CLASSES
+
+import pygame
+from pygame.locals import *
+from random import randint
+
+import constantes as const
+import assets
+
+pygame.init()
+
+class Fundo(pygame.sprite.Sprite):
+    def __init__(self, sprite):
+        super().__init__()
+
+        self.image = sprite
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (0, 0)
